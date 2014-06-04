c93b58da314bfcfa9bacd8a0313ddf1c6bee2add
Ian Schneider <planablediglet@gmail.com>
2014-06-04 14:25:11 -0600

    use unified GeoPkg from jeo and implement Backend

--- src/test/java/org/jeo/android/geopkg/GeoPkgWorkspaceTest.java
+++ src/test/java/org/jeo/android/geopkg/GeoPkgWorkspaceTest.java
@@ -21,7 +21,7 @@ public class GeoPkgWorkspaceTest {
 
     @Test
     public void testPrimaryKeyRegex() {
-        String parsed = GeoPkgWorkspace.parsePrimaryKeyColumn("CREATE TABLE \"urban_areas\" ( \"fid\" INTEGER PRIMARY KEY, \"the_geom\" BLOB, \"scalerank\" INTEGER, \"featurecla\" TEXT, \"area_sqkm\" REAL )");
+        String parsed = SQLiteBackend.parsePrimaryKeyColumn("CREATE TABLE \"urban_areas\" ( \"fid\" INTEGER PRIMARY KEY, \"the_geom\" BLOB, \"scalerank\" INTEGER, \"featurecla\" TEXT, \"area_sqkm\" REAL )");
         assertEquals("fid", parsed);
     }
 
